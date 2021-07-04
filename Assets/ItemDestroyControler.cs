55ea42d65e8c6ebef2fe63e0d978996350b7c693
Tokakiboshi <84213099+Tokakiboshi@users.noreply.github.com>
2021-07-04 14:24:23 +0900

    異常なし

---
+++ Assets/ItemDestroyControler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+        GameObject mainCamObj;
+
+
+    void Start()
+    {
+            mainCamObj = Camera.main.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mainCamObj.transform.position.z>gameObject.transform.position.z)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
